839aca415514a3b1c7be45bec22d4a08ad7d7582
commenthol <commenthol@gmail.com>
2020-08-19 13:09:40 +0200

    fix(base): same label on same page causes same id

--- src/base/utils/hooks.js
+++ src/base/utils/hooks.js
@@ -4,9 +4,7 @@ import { randomId } from './random-id'
 export const useId = function (prefix, props) {
   const idToUse = props.id
     ? props.id
-    : props.label
-      ? `${prefix}-${props.label}`
-      : randomId(prefix)
+    : randomId(prefix)
   const [id] = useState(idToUse)
   return id
 }

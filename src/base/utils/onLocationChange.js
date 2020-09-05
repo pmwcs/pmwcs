b6ab0280b547e1ff7eca06e31563d08415b13420
commenthol <commenthol@gmail.com>
2020-09-05 16:52:01 +0200

    feat(base): add onLocationChange history wrapper

---
+++ src/base/utils/onLocationChange.js
@@ -0,0 +1,27 @@
+/**
+ * wrapper around history functions to create custom event
+ * 'locationchange'
+ */
+export function onLocationChange () {
+  if (onLocationChange.wrapped) return
+
+  onLocationChange.wrapped = true
+
+  history.pushState = (f => function pushState(...args) {
+    const ret = f.apply(this, args)
+    window.dispatchEvent(new Event('pushstate'))
+    window.dispatchEvent(new Event('locationchange'))
+    return ret;
+  })(history.pushState)
+
+  history.replaceState = (f => function replaceState(...args) {
+    const ret = f.apply(this, args)
+    window.dispatchEvent(new Event('replacestate'));
+    window.dispatchEvent(new Event('locationchange'));
+    return ret;
+  })(history.replaceState)
+
+  window.addEventListener('popstate', () => {
+    window.dispatchEvent(new Event('locationchange'))
+  })
+}
